c426c34887c3e45bb4abec953a9dc17ce8494204
Sergio Alvarez-Napagao <tranchis@gmail.com>
2010-04-17 03:24:23 +0200

    Finished wikipedia to neighbourhood.

--- JavaParser/src/org/mapcelona/javaparser/barrios/Wikipedia2Neighbourhoods.java
+++ JavaParser/src/org/mapcelona/javaparser/barrios/Wikipedia2Neighbourhoods.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Source;
 import net.htmlparser.jericho.StartTag;
 
+import org.mapcelona.javaparser.rdfutils.RDFUtils;
+
 public class Wikipedia2Neighbourhoods
 {
-	public static void main(String args[]) throws MalformedURLException, IOException
+	public static void main(String args[]) throws MalformedURLException,
+			IOException
 	{
 		Source				s;
 		Iterator<Element>	it, itb;
@@ -20,35 +24,46 @@ public class Wikipedia2Neighbourhoods
 		Element				e;
 		StartTag			st;
 		List<Element>		l;
-		
-		s = new Source(new URL("http://ca.wikipedia.org/wiki/Districtes_i_barris_de_Barcelona"));
+		RDFUtils			ru;
+		List<String>		barrios;
+		String				name;
+
+		ru = new RDFUtils();
+		ru.clean();
+
+		s = new Source(
+				new URL(
+						"http://ca.wikipedia.org/wiki/Districtes_i_barris_de_Barcelona"));
 		it = s.getChildElements().iterator();
-//		while(it.hasNext())
-//		{
-			it.next(); // <!DOCTYPE>
-			e = it.next(); // <html>
-			its = e.getAllStartTags().iterator();
+
+		it.next(); // <!DOCTYPE>
+		e = it.next(); // <html>
+		its = e.getAllStartTags().iterator();
+		st = its.next();
+		while (its.hasNext() && !st.getName().equals("table"))
+		{
 			st = its.next();
-			while(its.hasNext() && !st.getName().equals("table"))
+		}
+
+		// Tenemos la tabla de distritos
+		it = st.getChildElements().get(0).getChildElements().iterator();
+		it.next(); // Nos saltamos la cabecera
+		while (it.hasNext())
+		{
+			e = it.next();
+			l = e.getChildElements();
+			name = l.get(1).getChildElements().get(0).getChildElements()
+				.get(0).getContent().toString();
+			l = l.get(5).getChildElements(); // Barrios
+			barrios = new LinkedList<String>();
+			itb = l.iterator();
+			while (itb.hasNext())
 			{
-				st = its.next();
+				barrios.add(itb.next().getContent().toString());
 			}
-			
-			// Tenemos la tabla de distritos
-			it = st.getChildElements().get(0).getChildElements().iterator();
-			it.next(); // Nos saltamos la cabecera
-			while(it.hasNext())
-			{
-				e = it.next();
-				l = e.getChildElements();
-				System.out.println("Nombre: " + l.get(1).getChildElements().get(0).getChildElements().get(0).getContent());
-				l = l.get(5).getChildElements(); // Barrios
-				itb = l.iterator();
-				while(itb.hasNext())
-				{
-					System.out.println(itb.next().getContent());
-				}
-			}
-//		}
+			ru.addDistrict(name, barrios);
+		}
+		
+		ru.dump();
 	}
 }
