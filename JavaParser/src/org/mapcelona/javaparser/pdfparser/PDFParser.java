738d86cadf2d9e14e14750bf3c93f2fa15dc306a
Sergio Alvarez-Napagao <tranchis@gmail.com>
2010-04-17 15:40:41 +0200

    Ya saca datos en RDF!

--- JavaParser/src/org/mapcelona/javaparser/pdfparser/PDFParser.java
+++ JavaParser/src/org/mapcelona/javaparser/pdfparser/PDFParser.java
@@ -5,7 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 import org.mapcelona.javaparser.rdfutils.RDFUtils;
 
@@ -22,15 +26,20 @@ public class PDFParser
 
 	public static void main(String args[]) throws Exception
 	{
-		File			temp;
-		ThExtract		te;
-		FileInputStream	fis;
-		BufferedReader	br;
-		String			line, district, b;
-		List<String>	barrios;
-		RDFUtils		ru;
-		Resource		r;
-		int				i;
+		File				temp;
+		ThExtract			te;
+		FileInputStream		fis;
+		BufferedReader		br;
+		String				line, district, b, data;
+		float				value;
+		List<String>		barrios;
+		RDFUtils			ru;
+		Resource			r, dataPiece;
+		int					i, year;
+		StringTokenizer		st;
+		Stack<String>		pila;
+		List<Resource>		pieces;
+		Iterator<Resource>	it;
 		
 		ru = new RDFUtils();
 		
@@ -78,16 +87,64 @@ public class PDFParser
 		
 		while(line != null)
 		{
+			pieces = new LinkedList<Resource>();
+			if(line.startsWith("% alumnes centres pœblics 2003-2004 37,1 54,6 - - - - -"))
+			{
+				line = line.replace("% alumnes centres pœblics 2003-2004 37,1 54,6 - - - - -",
+						"% alumnes centres pœblics 2003-2004 37,1 54,6 - - - - - -");
+			}
 			r = ru.checkMapping(line.substring(0, line.indexOf("20") - 1).replace("Þ", "fi"));
+			
+			st = new StringTokenizer(line);
+			pila = new Stack<String>();
+			
+			while(st.hasMoreTokens())
+			{
+				pila.push(st.nextToken());
+			}
 			
 			i = barrios.size() - 1;
 			while(i >= 0)
 			{
-				System.out.println(barrios.get(i));
+				data = pila.pop();
+				if(!data.equals("-"))
+				{
+					value = Float.parseFloat(data.replace(".", "").replace(",", "."));
+					System.out.println(barrios.get(i) + ":" + value);
+					dataPiece = ru.addDataPiece(barrios.get(i), r, value);
+					pieces.add(dataPiece);
+				}
+								
 				i = i - 1;
 			}
 			
+			data = pila.pop(); // Distrito
+			value = Float.parseFloat(data.replace(".", "").replace(",", "."));
+			System.out.println(district + ":" + value);
+			dataPiece = ru.addDataPiece(district, r, value);
+			
+			pila.pop(); // Barcelona
+			data = pila.pop(); // Year
+			if(data.indexOf('-') > -1)
+			{
+				year = Integer.parseInt(data.substring(data.lastIndexOf("-") + 1));
+			}
+			else
+			{
+				year = Integer.parseInt(data);
+			}
+			
+			it = pieces.iterator();
+			while(it.hasNext())
+			{
+				ru.setDataPieceYear(it.next(), year);
+			}
+			
+			System.out.println(r.getLocalName() + ":" + year);
+			
 			line = br.readLine();
 		}
+		
+		ru.dump();
 	}
 }
