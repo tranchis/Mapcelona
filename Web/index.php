8d9af0f71ea6dd66fe1e225bba5129320e56b6e6
unknown <Luis@.(none)>
2010-04-27 01:57:12 +0200

    Fixed issued with languages and parameters

--- Web/index.php
+++ Web/index.php
@@ -6,12 +6,11 @@ include('define.php');
 // If the users has chosen a different language, we set it accordingly in his session
 if(isset($_GET['lang']) && in_array($_GET['lang'],$langs)) $_SESSION['lang'] = $_GET['lang'];
 // The webpage will use the language chosen by the user according to his decission or his browser's preferences.
-if(isset($_SESSION['lang'])) $lang = $_SESSION['lang'];
-else {
-    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    if(!in_array($lang,$langs)) $lang = 'en';
+if(!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if(!in_array($_SESSION['lang'],$langs)) $_SESSION['lang'] = 'en';
 }
-include LANG_PATH.$lang.'.php';
+include LANG_PATH.$_SESSION['lang'].'.php';
 include_once('dataClassManager.php');
 $dcm = new dataClassManager();
 ?>
